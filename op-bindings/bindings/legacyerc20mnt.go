48d1b4d23bf5c9403006a99dc74e715aac50888d
liuhaoyang <lhy.0318@163.com>
2023-10-19 16:15:16 +0800

    update op-bindings

--- op-bindings/bindings/legacyerc20mnt.go
+++ op-bindings/bindings/legacyerc20mnt.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // LegacyERC20MNTMetaData contains all meta data concerning the LegacyERC20MNT contract.
@@ -156,11 +157,11 @@ func NewLegacyERC20MNTFilterer(address common.Address, filterer bind.ContractFil
 
 // bindLegacyERC20MNT binds a generic wrapper to an already deployed contract.
 func bindLegacyERC20MNT(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(LegacyERC20MNTABI))
+	parsed, err := LegacyERC20MNTMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
