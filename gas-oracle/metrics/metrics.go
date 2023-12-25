72ceb3c77e8bcda7f75ab8dad33826db6ffc399e
LeoZhang <njustzhangl@163.com>
2023-12-25 16:41:34 +0800

    [R4R]-[gas-oracle]feat: set token ratio scalar

--- gas-oracle/metrics/metrics.go
+++ gas-oracle/metrics/metrics.go
@@ -6,10 +6,14 @@ import (
 
 var (
 	GasOracleStats struct {
-		// metrics for L1 base fee, L1 bas price, da fee
+		// metrics for L1 base fee, L1 bas price
 		// TokenRatioGauge token_ratio = eth_price / mnt_price
 		TokenRatioGauge metrics.GaugeFloat64
-		// L1BaseFeeGauge (l1_base_fee + l1_priority_fee) * token_ratio
+		// TokenRatioWithScalarGauge token_ratio = token_ratio * token_ratio_scalar
+		TokenRatioWithScalarGauge metrics.GaugeFloat64
+		// TokenRatioOnchainGauge token_ratio on chain
+		TokenRatioOnchainGauge metrics.GaugeFloat64
+		// L1BaseFeeGauge
 		L1BaseFeeGauge metrics.Gauge
 		// FeeScalarGauge value to scale the fee up by
 		FeeScalarGauge metrics.Gauge
@@ -21,8 +25,9 @@ var (
 func InitAndRegisterStats(r metrics.Registry) {
 	metrics.Enabled = true
 
-	// stats for L1 base fee, L1 bas price, fee scalar
 	GasOracleStats.TokenRatioGauge = metrics.NewRegisteredGaugeFloat64("token_ratio", r)
+	GasOracleStats.TokenRatioWithScalarGauge = metrics.NewRegisteredGaugeFloat64("token_ratio_with_scalar", r)
+	GasOracleStats.TokenRatioOnchainGauge = metrics.NewRegisteredGaugeFloat64("token_ratio_onchain", r)
 	GasOracleStats.L1BaseFeeGauge = metrics.NewRegisteredGauge("l1_base_fee", r)
 	GasOracleStats.FeeScalarGauge = metrics.NewRegisteredGauge("fee_scalar", r)
 	GasOracleStats.L1GasPriceGauge = metrics.NewRegisteredGauge("l1_gas_price", r)
--- gas-oracle/oracle/l1_client.go
+++ gas-oracle/oracle/l1_client.go
@@ -1,14 +1,10 @@
 package oracle
 
 import (
-	"context"
 	"fmt"
-	"math/big"
 
-	ometrics "github.com/ethereum-optimism/optimism/gas-oracle/metrics"
 	"github.com/ethereum-optimism/optimism/gas-oracle/tokenratio"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/log"
 )
 
 type TokenRatioClient struct {
@@ -36,11 +32,4 @@ func NewTokenRatioClient(ethereumHttpUrl, layerTwoHttpUrl, tokenRatioCexURL, tok
 		l2Client:   l2Client,
 		tokenRatio: tokenRatio,
 	}, nil
-}
-
-func (c *TokenRatioClient) TokenRatio(ctx context.Context, number *big.Int) (float64, error) {
-	ratio := c.tokenRatio.TokenRatio()
-	log.Info("show base fee context", "ratio", ratio)
-	ometrics.GasOracleStats.TokenRatioGauge.Update(ratio)
-	return ratio, nil
 }
