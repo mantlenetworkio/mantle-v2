51488d8a37d4a1ebf0efb11892575a815efba85a
liuhaoyang <liuabel0318@gmail.com>
2023-12-27 14:45:09 +0800

    increase unsafeL2Payloads channel buffer size and enable batch try unsafe block

--- op-node/rollup/driver/driver.go
+++ op-node/rollup/driver/driver.go
@@ -141,7 +141,7 @@ func NewDriver(driverCfg *Config, cfg *rollup.Config, l2 L2Chain, l1 L1Chain, da
 		l1HeadSig:        make(chan eth.L1BlockRef, 10),
 		l1SafeSig:        make(chan eth.L1BlockRef, 10),
 		l1FinalizedSig:   make(chan eth.L1BlockRef, 10),
-		unsafeL2Payloads: make(chan *eth.ExecutionPayload, 10),
+		unsafeL2Payloads: make(chan *eth.ExecutionPayload, 1024),
 		altSync:          altSync,
 	}
 }
