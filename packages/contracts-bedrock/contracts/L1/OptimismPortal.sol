ca705b28cb256b978f852e800dabbb8b0fb52099
WangFeifan <wff19940326@gmail.com>
2024-03-06 18:27:43 +0800

    [R4R]-fix: N-06 Typographical Errors (#131)
    Co-authored-by: Ivan Wang <ivan.wang@mantle.xyz>

--- packages/contracts-bedrock/contracts/L1/OptimismPortal.sol
+++ packages/contracts-bedrock/contracts/L1/OptimismPortal.sol
@@ -29,7 +29,7 @@ contract OptimismPortal is Initializable, ResourceMetering, Semver {
      * @notice Represents a proven withdrawal.
      *
      * @custom:field outputRoot    Root of the L2 output this was proven against.
-     * @custom:field timestamp     Timestamp at whcih the withdrawal was proven.
+     * @custom:field timestamp     Timestamp at which the withdrawal was proven.
      * @custom:field l2OutputIndex Index of the output this was proven against.
      */
     struct ProvenWithdrawal {
@@ -70,7 +70,7 @@ contract OptimismPortal is Initializable, ResourceMetering, Semver {
 
     /**
      * @notice Address of the L2 account which initiated a withdrawal in this transaction. If the
-     *         of this variable is the default L2 sender address, then we are NOT inside of a call
+     *         value of this variable is the default L2 sender address, then we are NOT inside of a call
      *         to finalizeWithdrawalTransaction.
      */
     address public l2Sender;
