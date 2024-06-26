bf02201699a1265ae2befc1160f9f171fa75ebcc
adam.xu <adam.xu@mantle.xyz>
2024-06-26 14:25:41 +0800

    qa9 upgrade

--- op-service/upgrade/mantle_upgrade.go
+++ op-service/upgrade/mantle_upgrade.go
@@ -12,10 +12,11 @@ type UpgradeChainConfig struct {
 // chain config
 var (
 	// Mantle chain_id
-	MantleMainnetChainId   = big.NewInt(5000)
-	MantleSepoliaChainId   = big.NewInt(5003)
-	MantleSepoliaQAChainId = big.NewInt(5003003)
-	MantleLocalChainId     = big.NewInt(17)
+	MantleMainnetChainId    = big.NewInt(5000)
+	MantleSepoliaChainId    = big.NewInt(5003)
+	MantleSepoliaQAChainId  = big.NewInt(5003003)
+	MantleSepoliaQA9ChainId = big.NewInt(5003009)
+	MantleLocalChainId      = big.NewInt(17)
 )
 
 var (
@@ -30,6 +31,10 @@ var (
 	MantleSepoliaQAUpgradeConfig = UpgradeChainConfig{
 		ChainID:              MantleSepoliaQAChainId,
 		EigenDaUpgradeHeight: big.NewInt(3274000),
+	}
+	MantleSepoliaQA9UpgradeConfig = UpgradeChainConfig{
+		ChainID:              MantleSepoliaQA9ChainId,
+		EigenDaUpgradeHeight: big.NewInt(3411558 + 3600),
 	}
 	MantleLocalUpgradeConfig = UpgradeChainConfig{
 		ChainID:              MantleLocalChainId,
@@ -51,6 +56,8 @@ func GetUpgradeConfigForMantle(chainID *big.Int) *UpgradeChainConfig {
 		return &MantleSepoliaUpgradeConfig
 	case MantleSepoliaQAChainId.Int64():
 		return &MantleSepoliaQAUpgradeConfig
+	case MantleSepoliaQA9ChainId.Int64():
+		return &MantleSepoliaQA9UpgradeConfig
 	case MantleLocalChainId.Int64():
 		return &MantleLocalUpgradeConfig
 	default:
