a44ddb5e89af87d552b51c78480cf6009c95767b
adam.xu <adam.xu@mantle.xyz>
2024-10-14 15:14:54 +0800

    change test case location

--- op-node/rollup/derive/calldata_source_test.go
+++ op-node/rollup/derive/calldata_source_test.go
@@ -3,15 +3,12 @@ package derive
 import (
 	"context"
 	"crypto/ecdsa"
-	"encoding/hex"
 	"fmt"
 	"math/big"
 	"math/rand"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
-	"google.golang.org/protobuf/proto"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -26,7 +23,6 @@ import (
 	"github.com/ethereum-optimism/optimism/op-node/testlog"
 	"github.com/ethereum-optimism/optimism/op-node/testutils"
 	"github.com/ethereum-optimism/optimism/op-service/eigenda"
-	"github.com/ethereum-optimism/optimism/op-service/proto/gen/op_service/v1"
 )
 
 type testTx struct {
@@ -151,53 +147,6 @@ func TestRLPEncodeDecodeEthData(t *testing.T) {
 	var dataL = make([]eth.Data, 0)
 	err = rlp.DecodeBytes(bz, &dataL)
 	require.NoError(t, err)
-}
-
-func TestRetrieveBlob(t *testing.T) {
-	// da := eigenda.NewEigenDAClient(
-	cfg := eigenda.Config{
-		DisperserUrl:        "disperser-holesky.eigenda.xyz:443",
-		ProxyUrl:            "http://127.0.0.1:3100",
-		DisperseBlobTimeout: 20 * time.Minute,
-		RetrieveBlobTimeout: 20 * time.Minute,
-	}
-	// 	log.New(context.Background()),
-	// 	nil,
-	// )
-
-	calldata, _ := hex.DecodeString("ed1293040a20313594efb8d9721c7f000ba5dcc3e51a717e33662d318bb88c24896fda6473ff102218978a9a012202000128f2a442aa06de03010000f901d8f854f842a01805f5ef4df0dfc3d4ef78648e59b9888345e90ea6e8cdd5d662856782260517a000e4879b77e0e9542db302a559ef63944dcef02f5d1660bfb8f1793ba4b2c3938288dcccc480213740c6012137820400f9017f82b28d22f873eba0e6c0d26a4c0f30c76df3730f82e2f26d9015c43b90a61f260d0b0a8cb261c39d82000182416483268517a0c6cce1fadc0eaea692ce44586249c41b42befd001eb44363e7a7e130cafbe4bd008326856da0313594efb8d9721c7f000ba5dcc3e51a717e33662d318bb88c24896fda6473ffb901009bc91a9ff8246b9a3fd2efd49a48fa41add3eb78a208988f1497baae5837909faa4a8f58ed9cb9bd5bac32b7d2dd9483764d2af98b7752846f5677d4012864bcbbaa78187c8d4a7be2354515e8e25b4d7d9ee75b12581ca995437d6ca9e959d7767ef5a372b4587a4288aa16242217e1ab6252ac6193a88add75b6f7d9ca6f2f5125d73c4ebcadf3d82ab1f54d8fe2e91d4c193905b91fbbd9dd0fd30bd3fd2f5f2d5d7ff44d4baaabcb0f5e60d4217eceba6e3e4c2d160d9c7e78718d2018f26abb188a83feada8847ba19ad49eb97979a6e147eabf6a729b56903eda3722b574890eca80948cc8eff9eb9bd4037cc38b00f20136e66b756692734f403c337c820001")
-	calldataFrame := &op_service.CalldataFrame{}
-	err := proto.Unmarshal(calldata[1:], calldataFrame)
-	if err != nil {
-		return
-	}
-	frame := calldataFrame.Value.(*op_service.CalldataFrame_FrameRef)
-	da := da.NewEigenDADataStore(context.Background(), log.New("t1"), &cfg, nil, nil)
-	fmt.Printf("%x\n%x\n", frame.FrameRef.BatchHeaderHash, frame.FrameRef.Commitment)
-	data, err := da.RetrieveBlob(frame.FrameRef.BatchHeaderHash, frame.FrameRef.BlobIndex, nil)
-	if err != nil {
-		t.Errorf("RetrieveBlob err:%v", err)
-		return
-	}
-	fmt.Printf("RetrieveBlob data:%x\n", data)
-
-	data, err = da.RetrieveBlob(frame.FrameRef.BatchHeaderHash, frame.FrameRef.BlobIndex, frame.FrameRef.Commitment)
-	if err != nil {
-		t.Errorf("RetrieveBlob err:%v", err)
-		return
-	}
-
-	fmt.Printf("RetrieveBlob data:%x\n", data)
-
-	data = data[:frame.FrameRef.BlobLength]
-	outData := []eth.Data{}
-	err = rlp.DecodeBytes(data, &outData)
-	if err != nil {
-		log.Error("Decode retrieval frames in error,skip wrong data", "err", err, "blobInfo", fmt.Sprintf("%x:%d", frame.FrameRef.BatchHeaderHash, frame.FrameRef.BlobIndex))
-		return
-	}
-
-	fmt.Printf("RetrieveBlob %d\n", len(outData))
 }
 
 func TestRetrieveBlobTx(t *testing.T) {
