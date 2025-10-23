c187b82ce3ff2ce211771136c1114c0a5b67338a
Sebastian Stammler <seb@oplabs.co>
2025-10-23 15:17:33 +0200

    op-node/rollup: Drop non-empty batches for Jovian activation block (#17984)
    Also add and improve modular fork handling for the rollup config.

--- op-node/rollup/mantle_chain_spec.go
+++ op-node/rollup/mantle_chain_spec.go
@@ -1,5 +1,7 @@
 package rollup
 
+import "fmt"
+
 type MantleForkName string
 
 const (
@@ -36,4 +38,32 @@ func ForkToMantleFork(fork ForkName) MantleForkName {
 	default:
 		return MantleNone
 	}
+}
+
+var LatestMantleFork = AllMantleForks[len(AllMantleForks)-1]
+
+func MantleForksFrom(fork MantleForkName) []MantleForkName {
+	for i, f := range AllMantleForks {
+		if f == fork {
+			return AllMantleForks[i:]
+		}
+	}
+	panic(fmt.Sprintf("invalid mantle fork: %s", fork))
+}
+
+var nextMantleFork = func() map[MantleForkName]MantleForkName {
+	m := make(map[MantleForkName]MantleForkName, len(AllMantleForks))
+	for i, f := range AllMantleForks {
+		if i == len(AllMantleForks)-1 {
+			m[f] = MantleNone
+			break
+		}
+		m[f] = AllMantleForks[i+1]
+	}
+	return m
+}()
+
+func IsValidMantleFork(fork MantleForkName) bool {
+	_, ok := nextMantleFork[fork]
+	return ok
 }
