9987e7ba43114bcf1fa9bc2aa0acc3da2c384be2
sanbao <baobaobao369@163.com>
2025-11-03 16:46:05 +0800

    fix: zero min base fee and big block gas limit

--- op-node/cmd/genesis/cmd.go
+++ op-node/cmd/genesis/cmd.go
@@ -199,14 +199,14 @@ var Subcommands = cli.Commands{
 
 			// Mantle features
 			// setup initial 1559 params in rollup system config
-			if config.L2GenesisHoloceneTimeOffset == nil {
+			if config.L2GenesisMantleArsiaTimeOffset == nil {
 				rollupConfig.Genesis.SystemConfig.MarshalPreHolocene = true
 			}
-			if config.L2GenesisHoloceneTimeOffset != nil && *config.L2GenesisHoloceneTimeOffset == 0 {
+			if config.L2GenesisMantleArsiaTimeOffset != nil && *config.L2GenesisMantleArsiaTimeOffset == 0 {
 				rollupConfig.Genesis.SystemConfig.EIP1559Params = eth.Bytes8(eip1559.EncodeHolocene1559Params(config.EIP1559Denominator, config.EIP1559Elasticity))
 			}
-			if config.L2GenesisJovianTimeOffset != nil && *config.L2GenesisJovianTimeOffset == 0 {
-				rollupConfig.Genesis.SystemConfig.BaseFee = new(big.Int).SetUint64(config.MinBaseFee)
+			if config.L2GenesisMantleArsiaTimeOffset != nil && *config.L2GenesisMantleArsiaTimeOffset == 0 {
+				rollupConfig.Genesis.SystemConfig.MinBaseFee = config.MinBaseFee
 			}
 
 			if err := rollupConfig.Check(); err != nil {
