b6ec78fb86ebb7fde1dd2386aff03e53ad36e42b
ethan_chen <xichencx@163.com>
2026-01-21 16:14:45 +0800

    merge mantle-arsia

--- op-devstack/presets/mantle.go
+++ op-devstack/presets/mantle.go
@@ -6,9 +6,9 @@ import (
 	"github.com/ethereum-optimism/optimism/op-devstack/sysgo"
 )
 
-func WithMantleMinimal() stack.CommonOption {
-	return stack.MakeCommon(sysgo.DefaultMantleMinimalSystem(&sysgo.DefaultMinimalSystemIDs{}))
-}
+//func WithMantleMinimal() stack.CommonOption {
+//	return stack.MakeCommon(sysgo.DefaultMantleMinimalSystem(&sysgo.DefaultMinimalSystemIDs{}))
+//}
 
 func WithMantleArsiaAtGenesis() stack.CommonOption {
 	return stack.MakeCommon(sysgo.WithDeployerPipelineOption(sysgo.WithMantleForkAtGenesis(forks.MantleArsia)))
