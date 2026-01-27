dad4b5497f89676024e3a2bf48098a6b8a48d3d9
sanbao <baobaobao369@163.com>
2026-01-27 12:39:29 +0800

    refactor: gas oracle, exposed config and optimized metrics

--- gas-oracle/oracle/config.go
+++ gas-oracle/oracle/config.go
@@ -16,20 +16,20 @@ import (
 
 // Config represents the configuration options for the gas oracle
 type Config struct {
-	l1ChainID                       *big.Int
-	l2ChainID                       *big.Int
-	ethereumHttpUrl                 string
-	layerTwoHttpUrl                 string
-	gasPriceOracleAddress           common.Address
-	privateKey                      *ecdsa.PrivateKey
-	gasPrice                        *big.Int
-	waitForReceipt                  bool
-	tokenRatioEpochLengthSeconds    uint64
-	tokenRatioSignificanceFactor    float64
-	tokenRatioScalar                float64
-	tokenRatioCexURL                string
-	tokenRatioDexURL                string
-	tokenRatioUpdateFrequencySecond uint64
+	L1ChainID                       *big.Int
+	L2ChainID                       *big.Int
+	EthereumHttpUrl                 string
+	LayerTwoHttpUrl                 string
+	GasPriceOracleAddress           common.Address
+	PrivateKey                      *ecdsa.PrivateKey
+	GasPrice                        *big.Int
+	WaitForReceipt                  bool
+	TokenRatioEpochLengthSeconds    uint64
+	TokenRatioSignificanceFactor    float64
+	TokenRatioScalar                float64
+	TokenRatioCexURL                string
+	TokenRatioDexURL                string
+	TokenRatioUpdateFrequencySecond uint64
 	// hsm config
 	EnableHsm  bool
 	HsmAPIName string
@@ -44,16 +44,16 @@ type Config struct {
 // NewConfig creates a new Config
 func NewConfig(ctx *cli.Context) *Config {
 	cfg := Config{}
-	cfg.ethereumHttpUrl = ctx.String(flags.EthereumHttpUrlFlag.Name)
-	cfg.layerTwoHttpUrl = ctx.String(flags.LayerTwoHttpUrlFlag.Name)
+	cfg.EthereumHttpUrl = ctx.String(flags.EthereumHttpUrlFlag.Name)
+	cfg.LayerTwoHttpUrl = ctx.String(flags.LayerTwoHttpUrlFlag.Name)
 	addr := ctx.String(flags.GasPriceOracleAddressFlag.Name)
-	cfg.gasPriceOracleAddress = common.HexToAddress(addr)
-	cfg.tokenRatioCexURL = ctx.String(flags.TokenRatioCexURL.Name)
-	cfg.tokenRatioDexURL = ctx.String(flags.TokenRatioDexURL.Name)
-	cfg.tokenRatioUpdateFrequencySecond = ctx.Uint64(flags.TokenRatioUpdateFrequencySecond.Name)
-	cfg.tokenRatioEpochLengthSeconds = ctx.Uint64(flags.TokenRatioEpochLengthSecondsFlag.Name)
-	cfg.tokenRatioSignificanceFactor = ctx.Float64(flags.TokenRatioSignificanceFactorFlag.Name)
-	cfg.tokenRatioScalar = ctx.Float64(flags.TokenRatioScalarFlag.Name)
+	cfg.GasPriceOracleAddress = common.HexToAddress(addr)
+	cfg.TokenRatioCexURL = ctx.String(flags.TokenRatioCexURL.Name)
+	cfg.TokenRatioDexURL = ctx.String(flags.TokenRatioDexURL.Name)
+	cfg.TokenRatioUpdateFrequencySecond = ctx.Uint64(flags.TokenRatioUpdateFrequencySecond.Name)
+	cfg.TokenRatioEpochLengthSeconds = ctx.Uint64(flags.TokenRatioEpochLengthSecondsFlag.Name)
+	cfg.TokenRatioSignificanceFactor = ctx.Float64(flags.TokenRatioSignificanceFactorFlag.Name)
+	cfg.TokenRatioScalar = ctx.Float64(flags.TokenRatioScalarFlag.Name)
 	cfg.EnableHsm = ctx.Bool(flags.EnableHsmFlag.Name)
 	cfg.HsmAddress = ctx.String(flags.HsmAddressFlag.Name)
 	cfg.HsmAPIName = ctx.String(flags.HsmAPINameFlag.Name)
@@ -70,7 +70,7 @@ func NewConfig(ctx *cli.Context) *Config {
 			if err != nil {
 				log.Error(fmt.Sprintf("Option %q: %v", flags.PrivateKeyFlag.Name, err))
 			}
-			cfg.privateKey = key
+			cfg.PrivateKey = key
 		} else {
 			log.Crit("No private key configured")
 		}
@@ -78,20 +78,20 @@ func NewConfig(ctx *cli.Context) *Config {
 
 	if ctx.IsSet(flags.L1ChainIDFlag.Name) {
 		chainID := ctx.Uint64(flags.L1ChainIDFlag.Name)
-		cfg.l1ChainID = new(big.Int).SetUint64(chainID)
+		cfg.L1ChainID = new(big.Int).SetUint64(chainID)
 	}
 	if ctx.IsSet(flags.L2ChainIDFlag.Name) {
 		chainID := ctx.Uint64(flags.L2ChainIDFlag.Name)
-		cfg.l2ChainID = new(big.Int).SetUint64(chainID)
+		cfg.L2ChainID = new(big.Int).SetUint64(chainID)
 	}
 
 	if ctx.IsSet(flags.TransactionGasPriceFlag.Name) {
 		gasPrice := ctx.Uint64(flags.TransactionGasPriceFlag.Name)
-		cfg.gasPrice = new(big.Int).SetUint64(gasPrice)
+		cfg.GasPrice = new(big.Int).SetUint64(gasPrice)
 	}
 
 	if ctx.IsSet(flags.WaitForReceiptFlag.Name) {
-		cfg.waitForReceipt = true
+		cfg.WaitForReceipt = true
 	}
 
 	cfg.MetricsEnabled = ctx.Bool(flags.MetricsEnabledFlag.Name)
